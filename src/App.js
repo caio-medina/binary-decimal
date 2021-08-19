09ac408520198e7c7fd304e424c09d6720290b94
Caio <caio__medina@hotmail.com>
2021-08-19 16:26:51 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Components  from "./Components";
+
+function App() {
+  return (
+    <div className="Calculate">
+      <div>
+       
+       <Components />
+  
+      </div>
+      <h3>
+        Caio Medina !!!
+      </h3>
+ 
+    </div>
+  );
+}
+
+export default App;
